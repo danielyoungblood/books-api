5d5c3677edd7c7272cf306595ac9c8714636e7a7
daniel <danielyknechtli@gmail.com>
2023-06-02 15:36:37 -0700

    finished routes

--- controllers/books.js
+++ controllers/books.js
@@ -4,20 +4,33 @@ const getRoot = (req, res) => {
   try {
     db.Book.find()
       .then((books) => {
-        res.send(books);
+        res.status(200).json(books);
       })
       .catch((err) => {
+        res.status(500).json({
+          message: "show all books unsuccessful",
+        });
         console.log(err);
       });
   } catch (error) {
+    res.status(500).json({
+      message: "show all books unsuccessful",
+    });
     console.log(error);
   }
 };
 
 const getBook = (req, res) => {
-  db.Book.findById(req.params.id).then((book) => {
-    res.send(book);
-  });
+  try {
+    db.Book.findById(req.params.id).then((book) => {
+      res.status(200).json(book);
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "get book unsuccessful",
+    });
+    console.log(error);
+  }
 };
 
 const insertBooks = (req, res) => {
@@ -66,4 +79,61 @@ const insertBooks = (req, res) => {
     );
 };
 
-module.exports = { getRoot, getBook, insertBooks };
+const updateBook = (req, res) => {
+  console.log("router.put=>/:id, req.body" + req.body);
+  console.log("router.put=>/:id, req.params.id" + req.params.id);
+  db.Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.status(200).json({
+        message: "Book update successful",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Book update unsuccessful",
+      });
+      console.log("err", err);
+    });
+};
+
+const deleteBook = (req, res) => {
+  console.log("router.delete=>/:id, req.body" + req.body);
+  console.log("router.delete=>/:id, req.params.id" + req.params.id);
+  db.Book.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(200).json({
+        message: "Book delete successful",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Book deleted unsuccessful",
+      });
+      console.log("err", err);
+    });
+};
+
+const addBook = (req, res) => {
+  db.Book.create(req.body)
+    .then(() => {
+      res.status(200).json({
+        message: "Book added successful",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Book added unsuccessful",
+      });
+      console.log("err", err);
+    });
+};
+
+//!!THIS HAS TO BE THE VERY LAST LINE OF THE FILE!!
+module.exports = {
+  getRoot,
+  getBook,
+  insertBooks,
+  addBook,
+  deleteBook,
+  updateBook,
+};
--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,21 @@ const booksController = require("../controllers/books");
 const router = express.Router();
 
 router.get("/seed", booksController.insertBooks);
+
+//route for localhost:4000/books  (show all books)
 router.get("/", booksController.getRoot);
+
+//route for localhost:4000/books/<id>  (show a book)
 router.get("/:id", booksController.getBook);
 
+//route for localhost:4000/books/<id>  (update a book)
+router.put("/:id", booksController.updateBook);
+
+//route for localhost:4000/books/<id>   (delete a book)
+router.delete("/:id", booksController.deleteBook);
+
+//route for localhost:4000/books    (add a book)
+router.post("/", booksController.addBook);
+
+//!!THIS HAS TO BE THE VERY LAST LINE OF THE FILE!!
 module.exports = router;
